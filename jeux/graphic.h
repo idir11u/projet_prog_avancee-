4c2bd9a0188d70f0eec48593f242740f5a0e58de
idir11u <73847131+idir11u@users.noreply.github.com>
2020-11-15 04:43:54 +0100

    Create graphic.h

---
+++ jeux/graphic.h
@@ -0,0 +1,22 @@
+/**
+* \file  fonctios_SDL.h
+* \author  Ait Aider Zinedine && Idir Walid Hakim
+* \Brief bibliotheque graphic.h qui fait gestion de l'affichage dans le jeux  
+*/
+#ifndef GRAPHIC_H
+#define  GRAPHIC_H
+
+#include<SDL2/SDL.h>
+#include "fonctions_SDL.h"
+#include "world.h"
+
+void init_textures(world_t *world,SDL_Renderer * renderer);
+void handle_events(world_t *world,SDL_Event *evenements);
+void update_data(world_t *world);
+void refresh_graphic(world_t *world,SDL_Renderer * renderer);
+void clear_textures(world_t *world);
+
+
+
+
+#endif 
