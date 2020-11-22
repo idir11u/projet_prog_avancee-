8ec56d109731d45848137eb852fedc2a31279630
idir11u <73847131+idir11u@users.noreply.github.com>
2020-11-22 06:46:24 +0100

    Update graphic.c

--- jeux/graphic.c
+++ jeux/graphic.c
@@ -12,6 +12,7 @@ void init_textures(world_t *world,SDL_Renderer * renderer)
 	Uint8 r = 0, g = 255, b = 255;
 	world->terrain.image = charger_image_transparente("pavage.bmp", renderer,r,g,b);
 	world->heros.image = charger_image_transparente("sprite.bmp", renderer,r,g,b);
+	world->ennemy.image = charger_image_transparente("sprite.bmp", renderer,r,g,b);
 }
 
 void handle_events(world_t *world,SDL_Event *evenements)
@@ -28,40 +29,46 @@ void handle_events(world_t *world,SDL_Event *evenements)
 					case SDLK_q:
 						world->terminer = true;  break;
 					case SDLK_UP:
-						world->heros.DestR_sprite.y = world->heros.DestR_sprite.y - 4 ;
-						if (collision_murs(world->heros,world->terrain,world->ligne,world->colonne))
-							world->heros.DestR_sprite.y = ((world->heros.DestR_sprite.y/world->heros.DestR_sprite.h)+1)*world->heros.DestR_sprite.h;
-						update_farmes(&(world->heros.farme),NBR_HORIS_IMAGE_HEROS,1);// 
-						world->heros.SrcR_sprite.x = (LARGEUR_IMAGE_HEROS/NBR_HORIS_IMAGE_HEROS)*world->heros.farme.cpt;
-						world->heros.SrcR_sprite.y = HAUTEUR_IMAGE_HEROS-world->heros.SrcR_sprite.w ;
+						if (world->heros.est_visible == 1){
+							world->heros.DestR_sprite.y = world->heros.DestR_sprite.y - 4 ;
+							if (collision_murs(world->heros,world->terrain,world->ligne,world->colonne))
+								world->heros.DestR_sprite.y = ((world->heros.DestR_sprite.y/world->heros.DestR_sprite.h)+1)*world->heros.DestR_sprite.h;
+							update_farmes(&(world->heros.farme),NBR_HORIS_IMAGE_HEROS,1,2);// 
+							world->heros.SrcR_sprite.x = (LARGEUR_IMAGE_HEROS/NBR_HORIS_IMAGE_HEROS)*world->heros.farme.cpt;
+							world->heros.SrcR_sprite.y = HAUTEUR_IMAGE_HEROS-world->heros.SrcR_sprite.w ;
+						}
 						break;
-
 					case SDLK_DOWN: 
-						world->heros.DestR_sprite.y = world->heros.DestR_sprite.y + 4 ;
-						if (collision_murs(world->heros,world->terrain,world->ligne,world->colonne))
-							world->heros.DestR_sprite.y = (world->heros.DestR_sprite.y/world->heros.DestR_sprite.h)*world->heros.DestR_sprite.h;
-						update_farmes(&(world->heros.farme),NBR_HORIS_IMAGE_HEROS,0);// 
-						world->heros.SrcR_sprite.x = (LARGEUR_IMAGE_HEROS/NBR_HORIS_IMAGE_HEROS)*world->heros.farme.cpt;
-						world->heros.SrcR_sprite.y = 0;
+						if (world->heros.est_visible == 1){
+							world->heros.DestR_sprite.y = world->heros.DestR_sprite.y + 4 ;
+							if (collision_murs(world->heros,world->terrain,world->ligne,world->colonne))
+								world->heros.DestR_sprite.y = (world->heros.DestR_sprite.y/world->heros.DestR_sprite.h)*world->heros.DestR_sprite.h;
+							update_farmes(&(world->heros.farme),NBR_HORIS_IMAGE_HEROS,0,2);// 
+							world->heros.SrcR_sprite.x = (LARGEUR_IMAGE_HEROS/NBR_HORIS_IMAGE_HEROS)*world->heros.farme.cpt;
+							world->heros.SrcR_sprite.y = 0;
+						}	
 						break;
-
-					case SDLK_LEFT: 
-						world->heros.DestR_sprite.x  = world->heros.DestR_sprite.x - 4 ;  
-						if (collision_murs(world->heros,world->terrain,world->ligne,world->colonne))
-							world->heros.DestR_sprite.x = ((world->heros.DestR_sprite.x/world->heros.DestR_sprite.w)+1)*world->heros.DestR_sprite.w; // pour le coller au murs si il ya une collision avec le mur le plus 1 car c'est a gauche 
-						update_farmes(&(world->heros.farme),NBR_HORIS_IMAGE_HEROS,2);// 
-						world->heros.SrcR_sprite.x = (LARGEUR_IMAGE_HEROS/NBR_HORIS_IMAGE_HEROS)*world->heros.farme.cpt;
-						world->heros.SrcR_sprite.y =HAUTEUR_IMAGE_HEROS-3*world->heros.SrcR_sprite.w ;
+					case SDLK_LEFT:
+						if (world->heros.est_visible == 1){
+							world->heros.DestR_sprite.x  = world->heros.DestR_sprite.x - 4 ;  
+							if (collision_murs(world->heros,world->terrain,world->ligne,world->colonne))
+								world->heros.DestR_sprite.x = ((world->heros.DestR_sprite.x/world->heros.DestR_sprite.w)+1)*world->heros.DestR_sprite.w; // pour le coller au murs si il ya une collision avec le mur le plus 1 car c'est a gauche 
+							update_farmes(&(world->heros.farme),NBR_HORIS_IMAGE_HEROS,2,2);// 
+							world->heros.SrcR_sprite.x = (LARGEUR_IMAGE_HEROS/NBR_HORIS_IMAGE_HEROS)*world->heros.farme.cpt;
+							world->heros.SrcR_sprite.y =HAUTEUR_IMAGE_HEROS-3*world->heros.SrcR_sprite.w ;
+						}
 						break;
-
-					case SDLK_RIGHT:  
-						world->heros.DestR_sprite.x  = world->heros.DestR_sprite.x + 4 ; 
-						if (collision_murs(world->heros,world->terrain,world->ligne,world->colonne))
-								world->heros.DestR_sprite.x = (world->heros.DestR_sprite.x/world->heros.DestR_sprite.w)*world->heros.DestR_sprite.w; // pour le coller au murs si il ya une collision avec le mur 
-						update_farmes(&(world->heros.farme),NBR_HORIS_IMAGE_HEROS,2);// *
-						world->heros.SrcR_sprite.x = (LARGEUR_IMAGE_HEROS/NBR_HORIS_IMAGE_HEROS)*world->heros.farme.cpt;
-						world->heros.SrcR_sprite.y = HAUTEUR_IMAGE_HEROS-2*world->heros.SrcR_sprite.w ;
+					case SDLK_RIGHT: 
+						if (world->heros.est_visible == 1){
+							world->heros.DestR_sprite.x  = world->heros.DestR_sprite.x + 4 ; 
+							if (collision_murs(world->heros,world->terrain,world->ligne,world->colonne))
+									world->heros.DestR_sprite.x = (world->heros.DestR_sprite.x/world->heros.DestR_sprite.w)*world->heros.DestR_sprite.w; // pour le coller au murs si il ya une collision avec le mur 
+							update_farmes(&(world->heros.farme),NBR_HORIS_IMAGE_HEROS,3,2);// *
+							world->heros.SrcR_sprite.x = (LARGEUR_IMAGE_HEROS/NBR_HORIS_IMAGE_HEROS)*world->heros.farme.cpt;
+							world->heros.SrcR_sprite.y = HAUTEUR_IMAGE_HEROS-2*world->heros.SrcR_sprite.w ;
+						}
 						break;
+					
 				} 
 				
 			}
@@ -82,8 +89,10 @@ void refresh_graphic(world_t *world,SDL_Renderer * renderer)
 				SDL_RenderCopy(renderer,world->terrain.image, &(world->terrain.SrcR_terrain[i][j]),&(world->terrain.DestR_terrain[i][j]));
 			}
 		}
-		SDL_RenderCopy(renderer,world->heros.image, &(world->heros.SrcR_sprite), &(world->heros.DestR_sprite) );
-
+		if(world->heros.est_visible == 1) 
+			SDL_RenderCopy(renderer,world->heros.image, &(world->heros.SrcR_sprite), &(world->heros.DestR_sprite) );
+		if(world->ennemy.est_visible == 1) 
+			SDL_RenderCopy(renderer,world->ennemy.image, &(world->ennemy.SrcR_sprite), &(world->ennemy.DestR_sprite) );
 		SDL_RenderPresent(renderer);
 
 }
@@ -93,5 +102,7 @@ void clear_textures(world_t *world)
 {
 	SDL_DestroyTexture(world->terrain.image);
 	SDL_DestroyTexture(world->heros.image);
+	SDL_DestroyTexture(world->ennemy.image);
+
 }
 
