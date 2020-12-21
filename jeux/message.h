fe6f9446cd01619436889659167782a2b0289d64
idir11u <73847131+idir11u@users.noreply.github.com>
2020-12-21 01:40:11 +0100

    Update message.h

--- jeux/message.h
+++ jeux/message.h
@@ -5,26 +5,28 @@
 */
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 #include<SDL2/SDL.h>
 #include"fonctions_SDL.h"
 #include"affichage_text.h"
 
-
 #ifndef MESSAGE_H
 #define MESSAGE_H
-
 
 struct message_s
 {
 	text_t game_over;
 	text_t you_win;
+	text_t game_finished;
 	text_t score;
 	text_t score_chiffre;
 	text_t best_score[7];
+	text_t niveau[2];
+	bool est_affiche_niveau;
 };
 typedef struct message_s message_t;	
 
-void init_message(message_t *msg,SDL_Renderer* renderer,TTF_Font *font,int *best_score);
+void init_message(message_t *msg,SDL_Renderer* renderer,TTF_Font *font,int *best_score,int niveau);
 void update_message(text_t *text ,SDL_Renderer* renderer,TTF_Font *font,int chiffre,int x,int y, int h);
 void clean_message(message_t *msg);
 
