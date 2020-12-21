5e201957b860b255ec2994d6124f8d73c569fec9
idir11u <73847131+idir11u@users.noreply.github.com>
2020-12-21 01:38:45 +0100

    Update message.c

--- jeux/message.c
+++ jeux/message.c
@@ -10,10 +10,11 @@
 #include"constante.h"
 #include"message.h"
 
-void init_message(message_t *msg,SDL_Renderer* renderer,TTF_Font *font,int *tab)
+void init_message(message_t *msg,SDL_Renderer* renderer,TTF_Font *font,int *tab,int niveau)
 {
 	init_text(&(msg->game_over),"Game over",renderer,font,LARGEUR_ECRAN/4,HAUTEUR_ECRAN/4+HAUTEUR_ECRAN/8,LARGEUR_ECRAN/2,HAUTEUR_ECRAN/4); //HAUTEUR_ECRAN*(3/8)
 	init_text(&(msg->you_win),"You Win",renderer,font,LARGEUR_ECRAN/4,HAUTEUR_ECRAN/4+HAUTEUR_ECRAN/8,LARGEUR_ECRAN/2,HAUTEUR_ECRAN/4);
+	init_text(&(msg->game_finished),"game_finished",renderer,font,LARGEUR_ECRAN/4,HAUTEUR_ECRAN/4+HAUTEUR_ECRAN/8,LARGEUR_ECRAN/2,HAUTEUR_ECRAN/4);
 	init_text(&(msg->score),"Score :",renderer,font,LARGEUR_ECRAN*5/8,0,LARGEUR_ECRAN/6,HAUTEUR_ECRAN/16);
 	init_text(&(msg->score_chiffre),"0",renderer,font,LARGEUR_ECRAN*5/8+LARGEUR_ECRAN/6,0,LARGEUR_ECRAN/24,HAUTEUR_ECRAN/16);
 	init_text(&(msg->best_score[0]),"The Bests Scores : ",renderer,font,LARGEUR_ECRAN/4,HAUTEUR_ECRAN/4,LARGEUR_ECRAN/2,HAUTEUR_ECRAN/8); 
@@ -23,6 +24,11 @@ void init_message(message_t *msg,SDL_Renderer* renderer,TTF_Font *font,int *tab)
 	update_message(&(msg->best_score[4]),renderer,font,tab[0],(LARGEUR_ECRAN*5)/16+LARGEUR_ECRAN/8,(HAUTEUR_ECRAN*7)/16,HAUTEUR_ECRAN/8);
 	update_message(&(msg->best_score[5]),renderer,font,tab[1],(LARGEUR_ECRAN*5)/16+LARGEUR_ECRAN/8,(HAUTEUR_ECRAN*9)/16,HAUTEUR_ECRAN/8);
 	update_message(&(msg->best_score[6]),renderer,font,tab[2],(LARGEUR_ECRAN*5)/16+LARGEUR_ECRAN/8,(HAUTEUR_ECRAN*11)/16,HAUTEUR_ECRAN/8);
+	init_text(&(msg->niveau[0]),"level : ",renderer,font,LARGEUR_ECRAN/4,HAUTEUR_ECRAN/4+HAUTEUR_ECRAN/8,LARGEUR_ECRAN/4,HAUTEUR_ECRAN/8);
+	update_message(&(msg->niveau[1]),renderer,font,niveau,LARGEUR_ECRAN/4+LARGEUR_ECRAN/4, HAUTEUR_ECRAN/4+HAUTEUR_ECRAN/8,HAUTEUR_ECRAN/8);
+	msg->est_affiche_niveau = false;
+
+
 }
 ///LARGEUR_ECRAN*5/8+LARGEUR_ECRAN/6,0,LARGEUR_ECRAN/24,HAUTEUR_ECRAN/16);
 void update_message(text_t *text ,SDL_Renderer* renderer,TTF_Font *font,int chiffre,int x,int y,int h)
@@ -49,7 +55,9 @@ void clean_message(message_t *msg)
 	for(int i = 0 ; i<7 ; i++){
 		SDL_DestroyTexture(msg->best_score[i].text);
 	}
-
+	for(int i = 0 ; i<2 ; i++){
+		SDL_DestroyTexture(msg->niveau[i].text);
+	}
 
 }
 
