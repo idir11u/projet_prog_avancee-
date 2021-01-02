b8259e0b4c7c5cf75f4271396636db4e5e2aa9ee
idir11u <73847131+idir11u@users.noreply.github.com>
2021-01-02 03:22:12 +0100

    Update graphic.h

--- jeux/graphic.h
+++ jeux/graphic.h
@@ -8,6 +8,7 @@
 
 #include<SDL2/SDL.h>
 #include "fonctions_SDL.h"
+#include <SDL2/SDL_mixer.h>
 #include "world.h"
 #include "message.h"
 #include"jeu.h"
@@ -15,9 +16,9 @@
 
 
 void init_textures(jeu_t *jeu,world_t *world,SDL_Renderer * renderer);
-void handle_events(jeu_t *jeu,world_t *world,SDL_Event *evenements,SDL_Renderer * renderer,TTF_Font *font,message_t *msg,bruitages_t *bruits);
+void handle_events(jeu_t *jeu,world_t *world,SDL_Event *evenements,SDL_Renderer * renderer,TTF_Font *font,message_t *msg,bruitages_t* bruits);
 void update_data(world_t *world);
-void refresh_graphic(jeu_t *jeu,world_t *world,SDL_Renderer * renderer,TTF_Font *font,message_t *msg,bruitages_t *bruits);
+void refresh_graphic(jeu_t *jeu,world_t *world,SDL_Renderer * renderer,TTF_Font *font,message_t *msg,bruitages_t* bruits);
 void clear_textures(jeu_t *jeu,world_t *world);
 
 
