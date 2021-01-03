df998772ac41c3c9b7a6b0190d42741f6f91ea6f
idir11u <73847131+idir11u@users.noreply.github.com>
2021-01-03 10:22:06 +0100

    Update files.c

--- jeux/files.c
+++ jeux/files.c
@@ -2,6 +2,11 @@
 #include<stdlib.h>
 #include "headers/files.h"
 
+
+/**
+*Fonction de création d'une file vide 
+*@return une file vide 
+*/
 File file_vide()
 {	
 	File f=malloc(sizeof(f));
@@ -13,14 +18,22 @@ File file_vide()
 /*<************************************************************************>*/
 
 
-
+/**
+*Fonction détérminant si une file est vide 
+*@param f la file
+*@return l'etat de la file 0 si vide 1 sinon 
+*/
 int est_vide_file(File f)
 {
 	return (f->premier==NULL /*&& f->dernier==NULL*/);
 }
 /*<************************************************************************>*/
 
-
+/**
+* Fonction permettant d'enfiler un élement
+*@param f la file 
+*@param x l'élément à enfiler
+*/
 void enfiler(File f, int x)
 {
 	file_Element_t* elem=malloc(sizeof(*elem));
@@ -49,7 +62,10 @@ void enfiler(File f, int x)
 
 
 
-
+/**
+* Fonction qui éfface le contenue d'une file 
+*@param f la file
+*/
 void effacer_file(File f)
 {	
 	file_Element_t* tmp=NULL;
@@ -69,20 +85,31 @@ void effacer_file(File f)
 
 }
 
-
+/**
+*Détérmination de la tête de file
+*@param f la file 
+*@return la tete de file
+*/
 int tete_file(File f)
 {
 	return f->premier->element;
 }
 
 
-
+/**
+*Recherche de la queue de la file
+*@param f la file 
+*@return l'élément en queue de file 
+*/
 int queue_file(File f)
 {
 	return f->dernier->element;
 }
 
-
+/**
+* Afficher les éléments d'une file
+*@param f la file à afficher 
+*/
 void afficher_file(File f)
 {
 	file_Element_t* temp=f->premier;
@@ -96,7 +123,10 @@ void afficher_file(File f)
 	printf("\n");
 }
 
-
+/**
+*Défiler un élement de la file
+*@param f la file
+*/
 void defiler(File f)
 {	
 	if(est_vide_file(f))
@@ -111,7 +141,11 @@ void defiler(File f)
 	free(temp);
 }
 
-
+/**
+*Calcul de la longeur d'une file
+*@param f la file dont on veut déterminer la longueur
+*@return la longueur de la file
+*/
 int longueur_file(File f)
 {
 	int longueur=0;
